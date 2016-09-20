b2f120980390ce566af8bac3743d0e9badb77572
sxycxwb@163.com <sxycxwb@163.com>
2016-09-20 23:19:31 +0800

    提交用户登录主页

---
+++ project/DRP.Client.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿/*******************************************************************************
+ * Copyright © 2016 DRP.Framework 版权所有
+ * Author: XuWangbin
+ * Description: 计费运营系统
+ * Website：
+*********************************************************************************/
+using DRP.Application.SystemManage;
+using DRP.Client.Web;
+using DRP.Code;
+using DRP.Domain.Entity.SystemManage;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+
+namespace DRP.Web.Controllers
+{
+    //[HandlerLogin]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
