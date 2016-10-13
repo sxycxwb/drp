32ec1bc18cef8b3181e24be672a2933aa9ad84d4
sxycxwb@163.com <sxycxwb@163.com>
2016-10-13 21:16:12 +0800

    解决冲突

---
+++ project/DRP.Domain/03 Entity/DrpServManage/DrpProductModuelEntity.cs
@@ -0,0 +1,36 @@
+﻿using DRP.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRP.Domain.Entity.DrpServManage
+{
+    /// <summary>
+    /// 产品模块信息
+    /// </summary>
+    public class ProductModuleEntity : FullAuditedEntity<string>
+    {
+        /// <summary>
+        /// 产品ID
+        /// </summary>
+        public string F_ProductId { get; set; }
+
+        /// <summary>
+        /// 模块名称
+        /// </summary>
+        public string F_ModuleName { get; set; }
+
+        /// <summary>
+        /// 模块计费金额
+        /// </summary>
+        public decimal F_ChargeAmount { get; set; }
+
+        /// <summary>
+        /// 模块描述
+        /// </summary>
+        public string F_Description { get; set; }
+
+    }
+}
