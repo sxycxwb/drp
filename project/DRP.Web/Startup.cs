6cc57f14c10c379f7f556ecc89c6b7deac17a528
sxycxwb@163.com <sxycxwb@163.com>
2016-11-06 22:11:04 +0800

    集成Hangfire定时任务

---
+++ project/DRP.Web/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Hangfire;
+using Microsoft.Owin;
+using Owin;
+//using Hangfire.MySql;
+
+[assembly: OwinStartup(typeof(DRP.Web.Startup))]
+
+namespace DRP.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            //指定Hangfire使用内存存储后台任务信息
+            //GlobalConfiguration.Configuration.UseStorage(new MySqlStorage("Data Source=111.203.235.204;port=3306;Initial Catalog=hangfiremonitor;user id=drp;password=484950;Allow User Variables=True;"));
+            //启用HangfireServer这个中间件（它会自动释放）
+            app.UseHangfireServer();
+            //启用Hangfire的仪表盘（可以看到任务的状态，进度等信息）
+            app.UseHangfireDashboard();
+
+           // Hangfire.MySql.MySqlStorageConnection
+        }
+    }
+}
