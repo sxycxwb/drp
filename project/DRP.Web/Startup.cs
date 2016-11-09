8bc37d501d200c2f92aff3fd3ec16e989b76d67e
spring <xuwangbin@163.com>
2016-11-09 20:59:48 +0800

    添加Hangfire Mysql存储

--- project/DRP.Web/Startup.cs
+++ project/DRP.Web/Startup.cs
@@ -1,7 +1,8 @@
-﻿using Hangfire;
+﻿using System;
+using Hangfire;
 using Microsoft.Owin;
 using Owin;
-//using Hangfire.MySql;
+using Hangfire.MySql;
 
 [assembly: OwinStartup(typeof(DRP.Web.Startup))]
 
@@ -14,13 +15,19 @@ namespace DRP.Web
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
 
             //指定Hangfire使用内存存储后台任务信息
-            //GlobalConfiguration.Configuration.UseStorage(new MySqlStorage("Data Source=111.203.235.204;port=3306;Initial Catalog=hangfiremonitor;user id=drp;password=484950;Allow User Variables=True;"));
+            GlobalConfiguration.Configuration.UseStorage(new MySqlStorage("Data Source=111.203.235.204;port=3306;Initial Catalog=hangfiremonitor;user id=drp;password=484950;Allow User Variables=True;"));
             //启用HangfireServer这个中间件（它会自动释放）
+
+            ////使用内存，每次重启站点内容会丢失
+            //GlobalConfiguration.Configuration.UseMemoryStorage();
             app.UseHangfireServer();
             //启用Hangfire的仪表盘（可以看到任务的状态，进度等信息）
-            app.UseHangfireDashboard();
-
-           // Hangfire.MySql.MySqlStorageConnection
+            // Map Dashboard to the `http://<your-app>/hangfire` URL.
+            app.UseHangfireDashboard("/jobs");
+            BackgroundJob.Enqueue(() => Console.WriteLine("Hello, world!"));
+            BackgroundJob.Schedule(() => Console.WriteLine("Delayed"), TimeSpan.FromDays(1));
+            RecurringJob.AddOrUpdate(() => Console.Write("Recurring"), Cron.Daily);
+            // Hangfire.MySql.MySqlStorageConnection
         }
     }
 }
