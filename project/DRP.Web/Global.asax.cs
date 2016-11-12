fc843b8e407b173fd34e61147c842413a9e36275
sxycxwb@163.com <sxycxwb@163.com>
2016-11-13 00:38:12 +0800

    加入MinProfiler性能检测工具

--- project/DRP.Web/Global.asax.cs
+++ project/DRP.Web/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using StackExchange.Profiling;
+using StackExchange.Profiling.EntityFramework6;
 
 namespace DRP.Web
 {
@@ -14,6 +16,17 @@ namespace DRP.Web
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            MiniProfilerEF6.Initialize();
+        }
+
+        protected void Application_BeginRequest()
+        {
+            MiniProfiler.Start();
+        }
+
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Stop();
         }
     }
 }
---
+++ project/StackExchange.Profiling/WebRequestProfilerProvider.Settings.cs
@@ -0,0 +1,29 @@
+﻿namespace StackExchange.Profiling
+{
+    partial class WebRequestProfilerProvider
+    {
+        /// <summary>
+        /// WebRequestProfilerProvider specific configurations
+        /// </summary>
+        public static class Settings
+        {
+
+            static IUserProvider provider = new IpAddressIdentity();
+
+            /// <summary>
+            /// Provides user identification for a given profiling request.
+            /// </summary>
+            public static IUserProvider UserProvider
+            {
+                get
+                {
+                    return provider;
+                }
+                set
+                {
+                    provider = value;
+                }
+            }
+        }
+    }
+}
