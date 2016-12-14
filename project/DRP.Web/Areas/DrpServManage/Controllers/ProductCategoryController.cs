dfe48bb142dda33c6899157be8cbb4e55d60b058
spring <xuwangbin@163.com>
2016-12-14 18:07:02 +0800

    修改产品相关

---
+++ project/DRP.Web/Areas/DrpServManage/Controllers/ProductCategoryController.cs
@@ -0,0 +1,102 @@
+﻿/*******************************************************************************
+ * Copyright © 2016 DRP.Framework 版权所有
+ * Author: XuWangbin
+ * Description: 计费运营系统
+ * Website：
+*********************************************************************************/
+using DRP.Application.SystemManage;
+using DRP.Code;
+using DRP.Domain.Entity.SystemManage;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using DRP.Application.DrpServManage;
+using DRP.Domain.Entity.DrpServManage;
+
+namespace DRP.Web.Areas.DrpServManage.Controllers
+{
+    public class ProductCategoryController : ControllerBase
+    {
+        private ProductCategoryApp categoryApp = new ProductCategoryApp();
+
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public ActionResult GetTreeSelectJson()
+        {
+            var data = categoryApp.GetList();
+            var treeList = new List<TreeSelectModel>();
+            foreach (ProductCategoryEntity item in data)
+            {
+                TreeSelectModel treeModel = new TreeSelectModel();
+                treeModel.id = item.F_Id;
+                treeModel.text = item.F_FullName;
+                treeModel.parentId = item.F_ParentId;
+                treeList.Add(treeModel);
+            }
+            return Content(treeList.TreeSelectJson());
+        }
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public ActionResult GetTreeJson()
+        {
+            var data = categoryApp.GetList();
+            var treeList = new List<TreeViewModel>();
+            foreach (ProductCategoryEntity item in data)
+            {
+                TreeViewModel tree = new TreeViewModel();
+                bool hasChildren = data.Count(t => t.F_ParentId == item.F_Id) == 0 ? false : true;
+                tree.id = item.F_Id;
+                tree.text = item.F_FullName;
+                tree.value = item.F_EnCode;
+                tree.parentId = item.F_ParentId;
+                tree.isexpand = true;
+                tree.complete = true;
+                tree.hasChildren = hasChildren;
+                treeList.Add(tree);
+            }
+            return Content(treeList.TreeViewJson());
+        }
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public ActionResult GetTreeGridJson()
+        {
+            var data = categoryApp.GetList();
+            var treeList = new List<TreeGridModel>();
+            foreach (ProductCategoryEntity item in data)
+            {
+                TreeGridModel treeModel = new TreeGridModel();
+                bool hasChildren = data.Count(t => t.F_ParentId == item.F_Id) == 0 ? false : true;
+                treeModel.id = item.F_Id;
+                treeModel.isLeaf = hasChildren;
+                treeModel.parentId = item.F_ParentId;
+                treeModel.expanded = hasChildren;
+                treeModel.entityJson = item.ToJson();
+                treeList.Add(treeModel);
+            }
+            return Content(treeList.TreeGridJson());
+        }
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public ActionResult GetFormJson(string keyValue)
+        {
+            var data = categoryApp.GetForm(keyValue);
+            return Content(data.ToJson());
+        }
+        [HttpPost]
+        [HandlerAjaxOnly]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitForm(ProductCategoryEntity ProductCategoryEntity, string keyValue)
+        {
+            categoryApp.SubmitForm(ProductCategoryEntity, keyValue);
+            return Success("操作成功。");
+        }
+        [HttpPost]
+        [HandlerAjaxOnly]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteForm(string keyValue)
+        {
+            categoryApp.DeleteForm(keyValue);
+            return Success("删除成功。");
+        }
+    }
+}
